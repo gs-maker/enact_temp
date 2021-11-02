cf065b78b778dbfc1dd164e4c1e7d7e4714a00cb
gs-maker <gerald.ssali@gmail.com>
2021-11-02 11:23:37 +0300

    responsiveness added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import Services from "./components/Services";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
+import logo from "./img/enactLogo.svg";
 
 function App() {
 	return (
-		<div className="App">
-			<AboutUs />
-			<Services />
-			<ContactUs />
+		<div className="layout">
+			<div className="inner-layout">
+				<div className="logo-container"></div>
+
+				<div className="content">
+					<img src={logo} alt="enact logo" />
+					<AboutUs />
+					<Services />
+					<ContactUs />
+				</div>
+			</div>
 		</div>
 	);
 }
